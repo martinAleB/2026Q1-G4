a796f4c940724c0156ce8f03d98ef7404d079a43
martinAleB <martbar4@gmail.com>
2026-06-14 19:46:02 -0300

    Fix destroy script

--- scripts/terraform-destroy.sh
+++ scripts/terraform-destroy.sh
@@ -2,10 +2,11 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${SCRIPT_DIR}/env.sh"
 TF_DIR="${SCRIPT_DIR}/../terraform"
 ROOT_DIR="${SCRIPT_DIR}/.."
 
 mkdir -p "${ROOT_DIR}/backend/simulations/engine-dist"
 
 cd "${TF_DIR}"
-terraform destroy -auto-approve -var="bucket_name=${TF_FRONTEND_BUCKET_NAME}"
+terraform destroy -auto-approve
